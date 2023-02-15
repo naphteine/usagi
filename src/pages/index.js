e41194f764411b643634050f9a76363df51a6b29
gguilt <58526116+naphteine@users.noreply.github.com>
2023-02-15 18:30:35 +0300

    Moved logic to client-side

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-/*
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -41,45 +40,6 @@ const Home = () => {
       <Footer />
     </>
   )
-}
-
-export default Home
-*/
-
-import Link from "next/link";
-import { useEffect } from "react";
-import Footer from "../components/Footer"
-import Header from "../components/Header"
-import { supabase } from "../lib/supabaseClient";
-
-const Home = ({ captions }) => {
-  return (
-    <>
-      <Header/>
-
-      <main>
-        <ul>
-          {captions && captions.map((c) => {
-            return <li key={c.id}><Link href={`/baslik/${c.id}`}>{c.capt_text}</Link></li>
-          })}
-
-          {!captions && <div>Başlıklar bulunamadı</div>}
-        </ul>
-      </main>
-
-      <Footer />
-    </>
-  )
-}
-
-export async function getServerSideProps() {
-  let { data } = await supabase.from('captions').select()
-
-  return {
-    props: {
-      captions: data
-    },
-  }
 }
 
 export default Home
