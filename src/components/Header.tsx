c5c46fd92e83804659d6801e277e5db4fc3bd79c
Gökay Gültekin <58526116+naphteine@users.noreply.github.com>
2023-06-29 20:45:38 +0300

    Update color scheme and header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,22 @@
 import Link from "next/link";
 import styles from "../styles/Header.module.css";
+import rabbit from "../../public/rabbit.svg";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <Link href="/" className={styles.logo}>
-        usagisözlük
+        <Image priority src={rabbit} width={50} alt="usagisözlük" />
+        usagisozlük
       </Link>
+
+      <div className={styles.search}>
+        <form>
+          <input placeholder="Ara..."></input>
+          <button type="submit">Ara</button>
+        </form>
+      </div>
     </header>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,6 @@ import Header from "@/components/Header";
 import pb from "../../lib/pocketbase";
 
 export default function Home() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [arama, setArama] = useState(false);
   const [dictData, setDictData] = useState<ListResult>();
 
   async function getAll() {
@@ -24,36 +22,6 @@ export default function Home() {
     setDictData(resultList);
   }
 
-  async function makeSearch(term: any) {
-    const resultList = await pb.collection("usagi_captions").getList(1, 50, {
-      expand: "user",
-      filter: `name~"${term}"`,
-      $autoCancel: false,
-    });
-
-    setDictData(resultList);
-  }
-
-  function searched(e: any) {
-    e.preventDefault();
-    if (searchTerm.length > 0) {
-      setArama(true);
-      makeSearch(searchTerm);
-    } else {
-      getAll();
-      setArama(false);
-    }
-  }
-
-  function searchChange(e: any) {
-    if (e.target.value.length <= 0) {
-      getAll();
-      setArama(false);
-    } else {
-      setSearchTerm(e.target.value);
-    }
-  }
-
   useEffect(() => {
     getAll();
   }, []);
@@ -61,7 +29,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>UsagiSözlük</title>
+        <title>usagisözlük</title>
         <meta name="description" content="Güzel olan her şeyin sözlüğü" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -70,17 +38,7 @@ export default function Home() {
       <Header />
 
       <main className={styles.main}>
-        <div className={styles.search}>
-          <form onSubmit={searched}>
-            <input
-              onChange={searchChange}
-              onSubmit={searched}
-              placeholder="Ara..."
-            ></input>
-            <button type="submit">Ara</button>
-          </form>
-        </div>
-        {arama ? <h2>Arama sonuçları</h2> : <h2>Son eklenenler</h2>}
+        <h2>gündem</h2>
         {dictData?.items?.map((e) => (
           <DictEntry key={e.id} data={e} />
         ))}
